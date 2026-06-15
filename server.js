df31c8bb3a1fcb59d32ff6a1bcb24aac5e612a6f
Sharishma <chukkasharishma85@gmail.com>
2026-06-15 09:52:34 +0530

    Use Gmail service

--- server.js
+++ server.js
@@ -125,21 +125,11 @@ mongoose.connect(uri)
 ========================= */
 
 const transporter = nodemailer.createTransport({
-
-  host: "smtp.gmail.com",
-
-  port: 587,
-
-  secure: false,
-
+  service: "gmail",
   auth: {
-
     user: process.env.EMAIL_USER,
-
     pass: process.env.EMAIL_PASS
-
   }
-
 });
 const userSchema = new mongoose.Schema({
 
